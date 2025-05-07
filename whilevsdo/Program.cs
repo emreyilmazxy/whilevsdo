77f25a24e6380a198d23107e492962c3e238d67e
emreyilmaz <emreyilmazxy@gmail.com>
2025-05-07 23:17:02 +0300

    Add project files.

---
+++ whilevsdo/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("lütfen bir max-değer  belirtin ");
+int value = int.Parse(Console.ReadLine());
+int limit =1;
+while (limit <=value  )
+{
+
+    Console.WriteLine($"{limit} ben bir patikalıyım");
+    limit++;
+}
+
+//while döngüsü koşulu başta kontrol ettiği için hatalı girişlerde hiç çalışmaz ve daha güvenlidir. do...while en az bir kez çalıştığı için geçersiz girişlerde bile çıktı verir, bu da istenmeyen durumlara yol açabilir.
+
+Console.WriteLine("lütfen bir max-değer belirtin");
+int numberr = int.Parse(Console.ReadLine());
+
+int sinir = 1;
+do
+{
+
+    Console.WriteLine($"{sinir} ben bir patikalıyım");
+
+
+    sinir++;
+
+} while (sinir <= numberr);
